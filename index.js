3cd77c5e32c44a1e7f86ff04da886d54ad6f37a2
Miguel <miguel.ortiz.loaiza@gmail.com>
2026-09-05 11:23:01 -0500

    Comportamiento: Se arregló la lógica para el cambio de estado del aside entre distintas resoluciones - Se añadió un event listener para el cambio de resoluciones - A partir del event listener se modifican los atributos aria e inert correspondientes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
 const aside_button = document.getElementById("aside-toggle")
 const page = document.getElementById("page")
+const aside = document.getElementById("sidebar")
+
+// Media Query utilizada en el css para dispositivos móviles
+const mobileQuery = window.matchMedia("(max-width: 768px)")
+
+// Configuración inicial de los elementos y sus atributos
+// La página en móviles por defecto tiene el aside oculto y tiene inert para evitar interacciones con el teclado cuando el aside esta oculto
+// En otros dispositivos el aside está abierto y no se usa el inert, para tener acceso con el teclado en todo momento
 
 document.addEventListener("DOMContentLoaded", () => {
-    const isMobile = window.matchMedia("(max-width: 768px)").matches
+    const isMobile = mobileQuery.matches
 
-    if (!isMobile) page.classList.toggle("aside-open")
+    if (isMobile) aside.toggleAttribute("inert");
+    else page.classList.toggle("aside-open")
 
     aside_button.setAttribute("aria-expanded", String(!isMobile));
     aside_button.setAttribute(
@@ -14,6 +23,26 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 })
 
+// Cambiar el atributo inert del aside cuando se cambia el tamaño de la ventana
+mobileQuery.addEventListener("change", (event) => {
+    const isAsideOpen = page.classList.contains("aside-open")
+    if (event.matches) {
+        if (isAsideOpen) aside.removeAttribute("inert")
+        else aside.setAttribute("inert", true)
+    } else {
+        aside.removeAttribute("inert")
+    }
+})
+
+// Cambiar el estado del aside con el boton del header
 aside_button.addEventListener("click", () => {
-    page.classList.toggle("aside-open")
+    const isAsideOpen = page.classList.toggle("aside-open")
+    const isMobile = mobileQuery.matches
+    if (isMobile) aside.toggleAttribute("inert")
+
+    aside_button.setAttribute("aria-expanded", String(isAsideOpen));
+    aside_button.setAttribute(
+        "aria-label",
+        isAsideOpen ? "Contraer menú" : "Expandir menú"
+    );
 })
