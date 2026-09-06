da856dc77963c3b622a8e571bb287e8ecf53b3d6
Miguel <miguel.ortiz.loaiza@gmail.com>
2026-09-06 17:36:25 -0500

    Funcionamiento: se agregó la lógica para el cambio de secciones por medio del aside

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ const aside = document.getElementById("sidebar")
 // Media Query utilizada en el css para dispositivos móviles
 const mobileQuery = window.matchMedia("(max-width: 768px)")
 
-// Configuración inicial de los elementos y sus atributos
-// La página en móviles por defecto tiene el aside oculto y tiene inert para evitar interacciones con el teclado cuando el aside esta oculto
-// En otros dispositivos el aside está abierto y no se usa el inert, para tener acceso con el teclado en todo momento
-
-document.addEventListener("DOMContentLoaded", () => {
-    const isMobile = mobileQuery.matches
-
-    if (isMobile) aside.toggleAttribute("inert");
-    else page.classList.toggle("aside-open")
-
-    aside_button.setAttribute("aria-expanded", String(!isMobile));
-    aside_button.setAttribute(
-        "aria-label",
-        isMobile ? "Expandir menú" : "Contraer menú"
-    );
-})
-
 // Cambiar el atributo inert del aside cuando se cambia el tamaño de la ventana
 mobileQuery.addEventListener("change", (event) => {
     const isAsideOpen = page.classList.contains("aside-open")
@@ -45,4 +28,66 @@ aside_button.addEventListener("click", () => {
         "aria-label",
         isAsideOpen ? "Contraer menú" : "Expandir menú"
     );
+})
+
+// Configuración inicial de los elementos y sus atributos
+// La página en móviles por defecto tiene el aside oculto y tiene inert para evitar interacciones con el teclado cuando el aside esta oculto
+// En otros dispositivos el aside está abierto y no se usa el inert, para tener acceso con el teclado en todo momento
+
+document.addEventListener("DOMContentLoaded", () => {
+    const isMobile = mobileQuery.matches
+
+    if (isMobile) aside.toggleAttribute("inert");
+    else page.classList.toggle("aside-open")
+
+    aside_button.setAttribute("aria-expanded", String(!isMobile));
+    aside_button.setAttribute(
+        "aria-label",
+        isMobile ? "Expandir menú" : "Contraer menú"
+    );
+})
+
+// Lógica del cambio de secciones del dashboard
+
+const sectionButtons = document.querySelectorAll(
+    "aside button[data-section]"
+);
+
+const sections = document.querySelectorAll("main section");
+
+sectionButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+
+        const sectionId = button.dataset.section;
+
+        // Ocultar todas las secciones
+        sections.forEach((section) => {
+            section.hidden = true;
+        });
+
+        // Mostrar la sección seleccionada
+        document.getElementById(sectionId).hidden = false;
+
+        // Quitar el estado actual de todos los botones
+        sectionButtons.forEach((button) => {
+            button.removeAttribute("aria-current");
+        });
+
+        // Marcar el botón seleccionado
+        button.setAttribute("aria-current", "page");
+    });
+});
+
+
+// Botones adicionales del aside
+
+const notification_button = document.getElementById("notification-button")
+const user_button = document.getElementById("user-button")
+
+notification_button.addEventListener("click", () => {
+    alert("No tiene notificaciones nuevas.")
+})
+
+user_button.addEventListener("click", () => {
+    alert("Usuario administrador de la empresa ByteMarket. Dashboard informativo de las acciones presentadas en el periodo marzo-agosto de 2026.")
 })
